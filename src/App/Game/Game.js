8a0ee3674705be82c3a6737bd8e9dad5617f762c
N7_Remax <ditritusa@gmail.com>
2020-02-20 20:12:41 +0200

    fix

--- src/App/Game/Game.js
+++ src/App/Game/Game.js
@@ -48,7 +48,8 @@ class Game extends Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board squares={current.squares} />
+                    <Board squares={current.squares}
+                    onClick={(i)=> this.handleClick(i)} />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
