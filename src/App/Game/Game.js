9445f05635d3d5372f3c83dd71d529a79cef9b19
N7_Remax <ditritusa@gmail.com>
2020-02-20 20:32:17 +0200

    changed handleClick method in Game component

--- src/App/Game/Game.js
+++ src/App/Game/Game.js
@@ -26,7 +26,7 @@ class Game extends Component {
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -37,6 +37,7 @@ class Game extends Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
         });
     }
