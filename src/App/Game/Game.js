dae516f11817a409931ca39b26ba89b62a26cdf7
N7_Remax <ditritusa@gmail.com>
2020-02-20 20:29:44 +0200

    added jumpTo method and added history

--- src/App/Game/Game.js
+++ src/App/Game/Game.js
@@ -13,8 +13,16 @@ class Game extends Component {
             history: [{
                 squares: Array(9).fill(null),
             }],
+            stepNumber: 0,
             xIsNext: true,
         }
+    }
+
+    jumpTo(step){
+        this.state({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        })
     }
 
     handleClick(i) {
@@ -28,7 +36,7 @@ class Game extends Component {
         this.setState({
             history: history.concat([{
                 squares: squares,
-              }]),
+            }]),
             xIsNext: !this.state.xIsNext,
         });
     }
@@ -37,6 +45,16 @@ class Game extends Component {
         const history = this.state.history;
         const current = history[history.length - 1];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ? 'Перейти к ходу #' + move : 'К началу игры';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+
+        });
 
         let status;
         if (winner) {
@@ -49,11 +67,11 @@ class Game extends Component {
             <div className="game">
                 <div className="game-board">
                     <Board squares={current.squares}
-                    onClick={(i)=> this.handleClick(i)} />
+                        onClick={(i) => this.handleClick(i)} />
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
