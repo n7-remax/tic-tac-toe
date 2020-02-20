7de06569fdc85c9379ae8c620462bc666fdb2154
N7_Remax <ditritusa@gmail.com>
2020-02-20 18:38:19 +0200

    refactor board component

--- src/App/Board/Board.js
+++ src/App/Board/Board.js
@@ -5,8 +5,17 @@ import Square from '../Square/Square';
 import './board.css'
 
 class Board extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
     renderSquare(i) {
-        return <Square />;
+        return <Square 
+        value={this.state.squares[i]} 
+        onClick={()=> this.handleClick(i)} 
+        />;
     }
 
     render() {
