e91ae2073ca8b0d371bc9537af8c8c8bdf8927d6
N7_Remax <ditritusa@gmail.com>
2020-02-20 20:55:59 +0200

    Updates

---
+++ precache-manifest.07c4866228d0d7fd47cbda3b4c4bb235.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd07f149316a5277535a2aed5378f14c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "4c74a2421fcf13958fe5",
+    "url": "/tic-tac-toe/static/css/main.1a30e36c.chunk.css"
+  },
+  {
+    "revision": "d8b6ffee929070c962c8",
+    "url": "/tic-tac-toe/static/js/2.f473dba6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tic-tac-toe/static/js/2.f473dba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c74a2421fcf13958fe5",
+    "url": "/tic-tac-toe/static/js/main.9b8f62b1.chunk.js"
+  },
+  {
+    "revision": "fdbd6fae367d142c593b",
+    "url": "/tic-tac-toe/static/js/runtime-main.df4931a8.js"
+  }
+]);
