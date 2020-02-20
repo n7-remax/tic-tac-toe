25ede7eaff4c14bec4dc2ef08bd625da1d105298
N7_Remax <ditritusa@gmail.com>
2020-02-20 17:40:46 +0200

    added constructor to Square and added state and setState

--- src/App/Square/Square.js
+++ src/App/Square/Square.js
@@ -1,15 +1,24 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 
 import './square.css'
 
 class Square extends Component {
-    render() {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+        };
     }
-  }
+    render() {
+        return (
+            <button
+                className="square"
+                onClick={() => { this.setState({ value: 'X' }) }}
+                >
+                {this.state.value}
+            </button>
+        );
+    }
+}
 
-  export default Square
+export default Square
