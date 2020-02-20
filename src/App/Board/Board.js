9f48457f44f4efe6652e6745eed1373675e88bb7
N7_Remax <ditritusa@gmail.com>
2020-02-20 18:48:37 +0200

    added handleClick method in Board component

--- src/App/Board/Board.js
+++ src/App/Board/Board.js
@@ -11,6 +11,13 @@ class Board extends Component {
             squares: Array(9).fill(null),
         }
     }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+    
     renderSquare(i) {
         return <Square 
         value={this.state.squares[i]} 
